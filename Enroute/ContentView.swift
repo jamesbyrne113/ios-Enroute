7e2d35573499fed850896a681972eebb31678129
jamesbyrne113 <jamesbyrne113@gmail.com>
2020-08-27 15:21:28 +0100

    Lecture 11 and 12

---
+++ Enroute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enroute
+//
+//  Created by James Byrne on 26/08/2020.
+//  Copyright © 2020 jamesbyrne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
